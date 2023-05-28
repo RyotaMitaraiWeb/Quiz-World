e5f15465a4ae2f1371ed60e78794452ec711ffac
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-05-28 11:50:47 +0300

    add user state

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { IAppStore } from '../../types/store/store.types';
+import { userReducer } from './user/user.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot<IAppStore>(
+      {
+        user: userReducer,
+      }
+    ),
   ]
 })
 export class AppStoreModule { }
