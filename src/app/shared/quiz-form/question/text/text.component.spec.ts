379af46fa3b5f3be468bb8cf888f01b58e1e0c14
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-07 18:50:09 +0300

    refactor text question component to work with an injected form

--- src/app/shared/quiz-form/question/text/text.component.spec.ts
+++ src/app/shared/quiz-form/question/text/text.component.spec.ts
@@ -4,12 +4,31 @@ import { MatInputHarness } from '@angular/material/input/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TextComponent } from './text.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { FormArray } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 describe('TextComponent', () => {
   let component: TextComponent;
   let fixture: ComponentFixture<TextComponent>;
   let loader: HarnessLoader;
+
+  const event = new Event('click');
+  const fb = new FormBuilder();
+
+  let form = fb.group({
+    title: ['', [Validators.required, Validators.maxLength(100)]],
+    description: ['', [Validators.maxLength(300)]],
+    questions: fb.array(
+      [fb.group(
+        {
+          prompt: ['', [Validators.required, Validators.maxLength(100)]],
+          correctAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+          wrongAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+          type: ['text'],
+        }
+      )]
+    ),
+    instantMode: [false, [Validators.required]],
+  });
 
   describe('Unit tests', () => {
     beforeEach(() => {
@@ -19,68 +38,48 @@ describe('TextComponent', () => {
       fixture = TestBed.createComponent(TextComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
+
+      form = fb.group({
+        title: ['', [Validators.required, Validators.maxLength(100)]],
+        description: ['', [Validators.maxLength(300)]],
+        questions: fb.array(
+          [fb.group(
+            {
+              prompt: ['', [Validators.required, Validators.maxLength(100)]],
+              correctAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+              wrongAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+              type: ['text'],
+            }
+          )]
+        ),
+        instantMode: [false, [Validators.required]],
+      });
     });
 
     it('should create', () => {
       expect(component).toBeTruthy();
     });
-  });
 
-  describe('Integration tests', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [TextComponent, NoopAnimationsModule]
-      });
-      fixture = TestBed.createComponent(TextComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-
-    describe('hasMoreThanOneCorrectAnswerField getter', () => {
-      it('Returns true if the form has more than one correct answer', () => {
-        component.answers = [
-          {
-            value: 'a',
-            correct: true,
-          },
-          {
-            value: 'a',
-            correct: true,
-          },
-        ];
-
-        component.ngOnInit();
-
-        const result = component.hasMoreThanOneCorrectAnswerField;
-        expect(result).toBe(true);
-      });
-
-      it('Returns false if the form has exactly one correct answer', () => {
-        component.answers = [
-
-          {
-            value: 'a',
-            correct: true,
-          },
-        ];
-
-        component.ngOnInit();
-
-        const result = component.hasMoreThanOneCorrectAnswerField;
-        expect(result).toBe(false);
-      });
-    });
-
-    describe('addNewCorrectAnswerField', () => {
+    describe('addField', () => {
       it('Adds a new field to the form', () => {
         component.ngOnInit();
 
-        component.addNewCorrectAnswerField();
+        component.addField(event);
         const form = component.form.get('correctAnswers') as FormArray;
         expect(form.controls.length).toBe(2);
 
-        expect(form.controls[1].value).toEqual({ correctAnswer: ''});
+        expect(form.controls[1].value).toEqual({ answer: '' });
       });
+
+      describe('ngOnInit', () => {
+        it('Disables the wrong answers field', () => {
+          component.form = form.controls.questions.controls[0];
+          
+          expect(component.form.controls.wrongAnswers.disabled).toBe(false);
+          component.ngOnInit();
+          expect(component.form.controls.wrongAnswers.disabled).toBe(true);
+        });
+      })
     });
   });
 
@@ -98,47 +97,50 @@ describe('TextComponent', () => {
 
       questionEl = fixture.debugElement.nativeElement;
       loader = TestbedHarnessEnvironment.loader(fixture);
+      form = fb.group({
+        title: ['', [Validators.required, Validators.maxLength(100)]],
+        description: ['', [Validators.maxLength(300)]],
+        questions: fb.array(
+          [fb.group(
+            {
+              prompt: ['', [Validators.required, Validators.maxLength(100)]],
+              correctAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+              wrongAnswers: fb.array([fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })]),
+              type: ['single'],
+            }
+          )]
+        ),
+        instantMode: [false, [Validators.required]],
+      });
     });
 
     describe('Data populating', () => {
-      it('Renders input properties correctly', async () => {
-        component.prompt = 'Random Question';
-        component.answers = [
+      it('Renders correctly a passed form (wrong answers are removed)', async () => {
+        form.controls.questions.setValue([
           {
-            value: 'Correct answer',
-            correct: true,
+            prompt: 'question 1',
+            correctAnswers: [
+              { answer: 'correct' },
+            ],
+            wrongAnswers: [
+              { answer: 'wrong' }
+            ],
+            type: 'text',
           },
-          {
-            value: 'Correct answer2',
-            correct: true,
-          },
-        ];
+        ]);
 
+        form.controls.questions.controls[0].controls.correctAnswers.push(fb.group({ answer: 'correct2' }));
+
+        component.form = form.controls.questions.controls[0];
         component.ngOnInit();
-
         fixture.detectChanges();
 
         const fields = await loader.getAllHarnesses(MatInputHarness);
-        
         expect(fields.length).toBe(3);
 
-        expect(await fields[0].getValue()).toBe('Random Question');
-        expect(await fields[1].getValue()).toBe('Correct answer');
-        expect(await fields[2].getValue()).toBe('Correct answer2');
-
-        const correctFields = questionEl.querySelectorAll('.correct-answer-field');
-        expect(correctFields.length).toBe(2);
-      });
-
-      it('Renders input properly if no props are passed', async () => {
-        component.ngOnInit();
-        fixture.detectChanges();
-
-        const fields = await loader.getAllHarnesses(MatInputHarness);
-        expect(fields.length).toBe(2);
-
-        expect(await fields[0].getValue()).toBe('');
-        expect(await fields[1].getValue()).toBe('');
+        expect(await fields[0].getValue()).toBe('question 1');
+        expect(await fields[1].getValue()).toBe('correct');
+        expect(await fields[2].getValue()).toBe('correct2');
       });
     });
 
@@ -171,42 +173,41 @@ describe('TextComponent', () => {
         it('Adds a new field successfully', () => {
           component.ngOnInit();
           fixture.detectChanges();
-  
+
           const addBtn = questionEl.querySelector('.add-field-btn.correct') as HTMLElement;
           addBtn.click();
-  
+
           fixture.detectChanges();
-  
+
           const fields = questionEl.querySelectorAll('.correct-answer-field') as NodeListOf<HTMLInputElement>;
           expect(fields.length).toBe(2);
-  
+
           const field = fields[1];
           expect(field.value).toBe('');
         });
-  
+
         it('Removes a field successfully', () => {
           component.ngOnInit();
           fixture.detectChanges();
-  
+
           const addBtn = questionEl.querySelector('.add-field-btn.correct') as HTMLButtonElement;
           addBtn.click();
-  
+
           fixture.detectChanges();
-  
+
           const secondField = questionEl.querySelectorAll('.correct-answer-field')[1] as HTMLInputElement;
-  
+
           secondField.value = 'a';
-  
+
           fixture.detectChanges();
-  
+
           const removeBtn = questionEl.querySelector('.remove-field-btn.correct') as HTMLButtonElement;
           removeBtn.click();
-  
+
           fixture.detectChanges();
-  
+
           const remainingField = questionEl.querySelector('.correct-answer-field') as HTMLInputElement;
           expect(remainingField.value).toBe('a');
-  
         });
       });
     });
--- src/app/shared/quiz-form/question/text/text.component.ts
+++ src/app/shared/quiz-form/question/text/text.component.ts
@@ -1,12 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IAnswer } from '../../../../../types/components/answer.types';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IQuestion } from '../../../../../types/components/question.types';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { AnswersManager } from '../../../../util/AnswersManager/AnswersManager';
 
 @Component({
   selector: 'app-text',
@@ -24,112 +23,34 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 })
 export class TextComponent {
   constructor(private readonly fb: FormBuilder) {
-    this.form = this.fb.group({
-      prompt: [this.prompt, [Validators.required, Validators.maxLength(100)]],
-      correctAnswers: this.fb.array([
-        this.fb.group({ correctAnswer: ['', [Validators.required, Validators.maxLength(100)]] })
-      ]),
-      
-    });
-  }
-  ngOnInit(): void {
-    this.form.patchValue({
-      prompt: this.prompt,
-    });
-
-    const correctAnswersForm = this.form.get('correctAnswers') as FormArray;
-    while (correctAnswersForm.length) {
-      correctAnswersForm.removeAt(0);
-    }
-
-    this.correctAnswers.forEach(ca => {
-      const value = ca.value;
-      correctAnswersForm.push(this.fb.group({ correctAnswer: value }));
-    });
   }
 
-  @Input() prompt = '';
-  @Input() answers: IAnswer[] = [
-    {
-      value: '',
-      correct: true,
-    },
-    {
-      value: '',
-      correct: false,
-    }
-  ];
-
-  @Output() changeEvent = new EventEmitter<IQuestion>()
-
-  form = this.fb.group({
-    prompt: [this.prompt, [Validators.required, Validators.maxLength(100)]],
+  ngOnInit() {
+    this.answersManager = new AnswersManager(this.form.controls.correctAnswers, this.fb);
+    this.form.controls.wrongAnswers.disable();
+  }
+  
+  @Input({ required: true }) form = this.fb.group({
+    prompt: ['', [Validators.required, Validators.maxLength(100)]],
     correctAnswers: this.fb.array([
-      this.fb.group({ correctAnswer: ['', [Validators.required, Validators.maxLength(100)]] })
+      this.fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })
     ]),
+    wrongAnswers: this.fb.array([
+      this.fb.group({ answer: ['', [Validators.required, Validators.maxLength(100)]] })
+    ]),
+    type: ['text']
   });
+  
+  answersManager!: AnswersManager;
 
-  triggerChange() {
-    const values = this.form.value;
-    const prompt = values.prompt || '';
-    const answers: IAnswer[] = [];    
 
-    values.correctAnswers?.forEach((ca) => {
-      const value = ca.correctAnswer || '';
-
-      answers.push({
-        value,
-        correct: true,
-      });
-    });
-    
-    this.changeEvent.emit({
-      prompt,
-      answers,
-    });
+  addField(event: Event) {
+    event.preventDefault();
+    this.answersManager.addField();
   }
 
-  addNewCorrectAnswerField() {
-    const correctAnswersForm = this.form.get('correctAnswers') as FormArray;
-    correctAnswersForm.push(this.fb.group({ correctAnswer: ['', [Validators.required, Validators.maxLength(100)]] }));
-    this.triggerChange();
-  }
-
-  removeCorrectAnswerFieldAt(index: number) {
-    const correctAnswersForm = this.form.get('correctAnswers') as FormArray;
-
-    if (this.hasMoreThanOneCorrectAnswerField) {
-      correctAnswersForm.removeAt(index);
-      this.triggerChange();
-    }
-  }
-
-  correctAnswerFieldAtIndexErrors(index: number) {
-    const correctAnswersControls = this.form.controls.correctAnswers.controls
-    const errors = correctAnswersControls[index].controls.correctAnswer?.errors;
-    return errors;
-  }
-
-  get correctAnswersFormArray() {
-    const correctAnswers = this.form.get('correctAnswers') as FormArray;
-    const controls = correctAnswers.controls;
-
-    return controls;
-  }
-
-  get hasMoreThanOneCorrectAnswerField() {
-    return this.correctAnswersFormArray.length > 1;
-  }
-
-  private get correctAnswers(): IAnswer[] {
-    const answers = this.answers.filter(a => a.correct);
-    if (answers.length === 0) {
-      return [{
-        value: '',
-        correct: true,
-      }];
-    }
-
-    return answers;
+  removeField(event: Event, index: number) {
+    event.preventDefault();
+    this.answersManager.removeFieldAt(index);    
   }
 }
