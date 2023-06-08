014f5b99e3ceff58ff1457bef24cc7f960f5ddde
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-09 00:38:20 +0300

    fix a screw up

--- src/types/components/quiz-form.types.ts
+++ src/types/components/quiz-form.types.ts
@@ -5,31 +5,6 @@
     description: string;
     instantMode: boolean;
     questions: Partial<{
-      title: string | null;
-      description: string | null;
-      questions: Partial<{
-        prompt: string | null;
-        correctAnswers: Partial<{
-          answer: string | null;
-        }>[];
-        wrongAnswers: Partial<{
-          answer: string | null;
-        }>[];
-        type: string | null;
-      }>[];
-      instantMode: boolean | null;
-    }>;
-  }
- * ```
- */
-export interface IQuizFormSubmission {
-  title: string;
-  description: string;
-  instantMode: boolean;
-  questions: Partial<{
-    title: string | null;
-    description: string | null;
-    questions: Partial<{
       prompt: string | null;
       correctAnswers: Partial<{
         answer: string | null;
@@ -39,6 +14,21 @@ export interface IQuizFormSubmission {
       }>[];
       type: string | null;
     }>[];
-    instantMode: boolean | null;
-  }>;
+  }
+ * ```
+ */
+export interface IQuizFormSubmission {
+  title: string;
+  description: string;
+  instantMode: boolean;
+  questions: Partial<{
+    prompt: string | null;
+    correctAnswers: Partial<{
+      answer: string | null;
+    }>[];
+    wrongAnswers: Partial<{
+      answer: string | null;
+    }>[];
+    type: string | null;
+  }>[];
 }
