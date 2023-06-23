7a500c878f202226abe0a87ca47be4e6623982ea
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-23 23:15:29 +0300

    add register

--- src/app/core/auth-service/auth.service.spec.ts
+++ src/app/core/auth-service/auth.service.spec.ts
@@ -87,6 +87,59 @@ describe('AuthService', () => {
     });
   });
 
+  describe('register', () => {
+    it('Correctly handles a successful response', (done: DoneFn) => {
+      const res = service.register(body);
+      res.subscribe({
+        next: res => {
+          expect(res.status).toBe(HttpStatusCode.Created);
+          const user = res.body!;
+          expect(user.id).toBe(1);
+          expect(user.token).toBe('a');
+          expect(user.username).toBe('a');
+          done();
+        },
+        error: err => {
+          done.fail('Expected a successful response, not an error one');
+          console.warn(err);
+        }
+      });
+
+      const request = testController.expectOne(service.url.register);
+      request.flush(response, {
+        status: HttpStatusCode.Created,
+        statusText: 'Created',
+      });
+    });
+
+    it('Correctly handles an error response', (done: DoneFn) => {
+      const res = service.register(body);
+      res.subscribe({
+        next: res => {
+          done.fail('Expected an error response, not a successful one')
+        },
+        error: (err: HttpErrorResponse) => {
+          expect(err.status).toBe(HttpStatusCode.Unauthorized);
+          const errors = err.error;
+          expect(errors.message).toEqual(['error']);
+          done();
+        }
+      });
+
+      const errorResponse = new HttpErrorResponse({
+        status: HttpStatusCode.Unauthorized,
+        statusText: 'Unauthorized',
+      });
+
+      const request = testController.expectOne(service.url.register);
+      request.flush({
+        message: ['error']
+      }, {
+        status: HttpStatusCode.Unauthorized,
+        statusText: 'Unauthorized',
+      });
+    });
+  });
   afterEach(() => {
     testController.verify();
   });
