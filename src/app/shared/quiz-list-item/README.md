7a2c09defa9ab007d5650d905bccce50c420a1f6
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-30 13:48:44 +0300

    add QuizListItem component

--- src/app/shared/quiz-list-item/README.md
+++ src/app/shared/quiz-list-item/README.md
@@ -2,5 +2,16 @@
 
 ## Usage
 ```html
-<app-quiz-list-item></app-quiz-list-item>
+<app-quiz-list-item [quiz]="{}"></app-quiz-list-item>
 ```
+
+### @Input() properties
+```typescript
+class QuizListItemComponent {
+  @Input() quiz: IQuizListItem;
+}
+```
+
+* ``quiz`` - the quiz for which this card applies (**Required**)
+
+Renders a ``<mat-card>`` that contains details about a given quiz. This card is meant to be rendered in a list of quizzes (such as a list of all quizzes, search results, and similar). The description is shortened to 50 characters if it's longer than that.
--- src/app/shared/quiz-list-item/quiz-list-item.component.ts
+++ src/app/shared/quiz-list-item/quiz-list-item.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { RoleService } from '../../core/role-service/role.service';
+import { IQuizListItem } from '../../../types/others/lists.types';
+import { ShortenPipe } from '../pipes/shorten/shorten.pipe';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-quiz-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ShortenPipe,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './quiz-list-item.component.html',
   styleUrls: ['./quiz-list-item.component.scss']
 })
@@ -15,4 +27,12 @@ export class QuizListItemComponent {
     private readonly router: Router,
     private readonly roleService: RoleService,
   ) { }
+
+  @Input({ required: true }) quiz: IQuizListItem = {
+    id: 0,
+    title: '',
+    description: '',
+    instantMode: false,
+    createdOn: ''
+  };
 }
