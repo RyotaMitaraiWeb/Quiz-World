601c74f70c3d47994c2678853760554f581ef42d
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-30 11:17:58 +0300

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionModule } from './quiz-form/question/question.module';
 import { QuizFormModule } from './quiz-form/quiz-form.module';
+import { ShortenPipe } from './pipes/shorten/shorten.pipe';
+import { QuizListItemModule } from './quiz-list-item/quiz-list-item.module';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     QuestionModule,
     QuizFormModule,
+    ShortenPipe,
+    QuizListItemModule
   ],
-  exports: [QuestionModule, QuizFormModule]
+  exports: [
+    QuestionModule,
+    QuizFormModule,
+    QuizListItemModule,
+    ShortenPipe,
+  ],
 })
 export class SharedModule { }
