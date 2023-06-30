5ed6d48f59f986373f02f4148f795513097f5058
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-30 12:00:49 +0300

    implement shorten pipe functionality

--- src/app/shared/pipes/shorten/shorten.pipe.spec.ts
+++ src/app/shared/pipes/shorten/shorten.pipe.spec.ts
@@ -1,8 +1,26 @@
 import { ShortenPipe } from './shorten.pipe';
 
 describe('ShortenPipe', () => {
+  let pipe = new ShortenPipe();
   it('create an instance', () => {
-    const pipe = new ShortenPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('Returns the same string if lower than or equal to the default max length', () => {
+    const value = pipe.transform('a');
+    expect(value).toBe('a');
+
+    const value2 = pipe.transform('a'.repeat(50));
+    expect(value2).toBe('a'.repeat(50));
+  });
+
+  it('Returns a shortened string if longer than the default max length', () => {
+    const value = pipe.transform('a'.repeat(51));
+    expect(value).toBe('a'.repeat(50) + '...');
+  });
+
+  it('Returns a shortened string if longer than the provided max length', () => {
+    const value = pipe.transform('aaa', 1);
+    expect(value).toBe('a...');
   });
 });
