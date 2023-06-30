1dcb9635bfd15cf5efdb6acb8f7bf0792e5d7714
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-06-30 10:52:13 +0300

    add a new type for quiz list items

--- src/types/others/lists.types.ts
+++ src/types/others/lists.types.ts
@@ -1,2 +1,11 @@
 export type order = 'asc' | 'desc';
 export type sort = 'title' | 'createdOn' | 'updatedOn';
+
+export interface IQuizListItem {
+  id: number;
+  title: string;
+  description: string;
+  instantMode: boolean;
+  createdOn: string;
+  updatedOn: string;
+}
