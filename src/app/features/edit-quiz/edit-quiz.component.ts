604d877f2feca3de3aa736cd5ff4f47144e7ebab
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-25 22:43:30 +0300

    add edit quiz

---
+++ src/app/features/edit-quiz/edit-quiz.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { IEditQuizFormSubmission, IQuizFormSubmission } from '../../../types/components/quiz-form.types';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuizService } from '../quiz-service/quiz.service';
+import { IQuizDetails } from '../../../types/responses/quiz.types';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-edit-quiz',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SharedModule,
+  ],
+  templateUrl: './edit-quiz.component.html',
+  styleUrls: ['./edit-quiz.component.scss']
+})
+export class EditQuizComponent implements OnInit, OnDestroy {
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly quizService: QuizService,
+    private readonly router: Router,
+  ) {}
+  private quizSub = new Subscription();
+  private editSub = new Subscription();
+
+  ngOnInit(): void {
+    this.quizSub = this.getResolvedData().subscribe(data => {
+      this.quiz = data['quiz'];
+    });
+  }
+
+  editQuiz(quiz: IQuizFormSubmission): void {
+    this.editSub = this.quizService.edit(this.quiz.id, quiz).subscribe({
+      next: () => {
+        this.router.navigate(['/quiz', this.quiz.id.toString()])
+      },
+      error: (err) => {
+        console.warn(err);
+        
+      }
+    })
+  }
+
+  getResolvedData() {
+    return this.activatedRoute.data;
+  }
+
+  ngOnDestroy(): void {
+    this.quizSub.unsubscribe();
+  }
+
+  quiz: IEditQuizFormSubmission = {
+    id: 0,
+    title: '',
+    description: '',
+    questions: [],
+    
+  }
+
+}
