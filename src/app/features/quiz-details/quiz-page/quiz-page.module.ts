aae478e54cff8d119832fafe7f69582da9077ca6
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-25 13:48:21 +0300

    add barebones quiz page

---
+++ src/app/features/quiz-details/quiz-page/quiz-page.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuizPageComponent } from './quiz-page.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    QuizPageComponent
+  ],
+  exports: [
+    QuizPageComponent
+  ]
+})
+export class QuizPageModule { }
