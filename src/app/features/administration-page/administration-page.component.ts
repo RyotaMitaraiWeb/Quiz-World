ec6287a68393be0203bca08fadb18c0062d0e711
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-26 01:16:16 +0300

    add administration page skeleton

---
+++ src/app/features/administration-page/administration-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-administration-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './administration-page.component.html',
+  styleUrls: ['./administration-page.component.scss']
+})
+export class AdministrationPageComponent {
+
+}
