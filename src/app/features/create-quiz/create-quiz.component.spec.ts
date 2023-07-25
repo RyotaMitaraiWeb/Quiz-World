7a200b180f027c405e9f56d66ae6c953cf0ebb94
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-25 16:59:41 +0300

    add create quiz page

---
+++ src/app/features/create-quiz/create-quiz.component.spec.ts
@@ -0,0 +1,77 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { CreateQuizComponent } from './create-quiz.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { QuizService } from '../quiz-service/quiz.service';
+import { Router } from '@angular/router';
+import { ViewChild } from '@angular/core';
+import { QuizFormComponent } from '../../shared/quiz-form/quiz-form.component';
+import { Observable, of } from 'rxjs';
+import { HttpResponse, HttpStatusCode } from '@angular/common/http';
+import { IQuizFormSubmission } from '../../../types/components/quiz-form.types';
+
+describe('CreateQuizComponent', () => {
+  let component: CreateQuizComponent;
+  let fixture: ComponentFixture<CreateQuizComponent>;
+
+  let quizService: QuizService;
+  let router: Router;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        CreateQuizComponent,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
+    });
+    fixture = TestBed.createComponent(CreateQuizComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    quizService = TestBed.inject(QuizService);
+    router = TestBed.inject(Router);
+  });
+
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('Unit tests', () => {
+    describe('createQuiz', () => {
+      it('Calls the router navigate method if request is successful', waitForAsync(() => {
+        spyOn(quizService, 'create').and.returnValue(
+          of(new HttpResponse({
+            status: HttpStatusCode.Created,
+            statusText: 'Created',
+            body: { id: 1 }
+          }))
+        );
+
+        spyOn(router, 'navigate').and.stub();
+
+        component.createQuiz({} as IQuizFormSubmission);
+        expect(router.navigate).toHaveBeenCalledWith(['/quiz', '1']);
+      }));
+
+      it('Does not call the router navigate method if request is unsuccessful', waitForAsync(() => {
+        spyOn(quizService, 'create').and.returnValue(
+          new Observable(o => o.error(new HttpResponse(
+            {
+              status: HttpStatusCode.BadRequest,
+              statusText: 'Bad Request'
+            }
+          )))
+        );
+
+        spyOn(router, 'navigate').and.stub();
+
+        component.createQuiz({} as IQuizFormSubmission);
+        expect(router.navigate).not.toHaveBeenCalled();
+      }));
+    });
+  });
+});
---
+++ src/app/features/create-quiz/create-quiz.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { QuizService } from '../quiz-service/quiz.service';
+import { Router } from '@angular/router';
+import { IQuizFormSubmission } from '../../../types/components/quiz-form.types';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-create-quiz',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  templateUrl: './create-quiz.component.html',
+  styleUrls: ['./create-quiz.component.scss']
+})
+export class CreateQuizComponent implements OnDestroy {
+  private quizSub = new Subscription();
+
+  constructor(
+    private readonly quizService: QuizService,
+    private readonly router: Router,
+  ) { }
+
+  createQuiz(quiz: IQuizFormSubmission): void {
+    this.quizSub = this.quizService.create(quiz).subscribe({
+      next: (res) => {
+        const body = res.body;
+        this.router.navigate(['/quiz', body?.id.toString()]);
+      },
+      error: (err) => {
+        console.warn(err);
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.quizSub.unsubscribe();
+  }
+
+}
