3a2a59406eb61c54846920ba06996f4fc7ea3195
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-26 13:51:51 +0300

    initialize generic users list component

---
+++ src/app/features/administration-page/users-list/users-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, Subscription } from 'rxjs';
+import { AdminService } from '../../admin-service/admin.service';
+import { MatTableModule } from '@angular/material/table';
+import { IUserResponse } from '../../../../types/responses/administration.types';
+@Component({
+  selector: 'app-users-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule
+  ],
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.scss']
+})
+export class UsersListComponent {
+  @Input({ required: true }) data: IUserResponse[] | Observable<IUserResponse[]> = [];
+
+  protected displayedColumns = ['username', 'roles'];
+}
