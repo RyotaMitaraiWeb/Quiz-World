497cecd406a9820b935f83d2d9d18ff0ce72d395
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-26 12:51:20 +0300

    fix faulty test

--- src/app/features/administration-page/logs/logs.component.spec.ts
+++ src/app/features/administration-page/logs/logs.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogsComponent } from './logs.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppStoreModule } from '../../../store/app-store.module';
 
 describe('LogsComponent', () => {
   let component: LogsComponent;
@@ -8,7 +11,7 @@ describe('LogsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [LogsComponent]
+      imports: [LogsComponent, HttpClientTestingModule, NoopAnimationsModule, AppStoreModule]
     });
     fixture = TestBed.createComponent(LogsComponent);
     component = fixture.componentInstance;
