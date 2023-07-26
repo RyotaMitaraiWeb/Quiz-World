c20e7dbeb097829c769393fd6c314f8d7a4ca775
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-07-27 01:12:51 +0300

    add a few more initial features to logs tab

--- src/app/features/administration-page/logs/logs.component.ts
+++ src/app/features/administration-page/logs/logs.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminService } from '../../admin-service/admin.service';
 import { order } from '../../../../types/others/lists.types';
+import { SharedModule } from '../../../shared/shared.module';
+import { SorterComponent } from '../sorter/sorter.component';
+import { MatTableModule } from '@angular/material/table';
+import { IIndexedLogActivity } from '../../../../types/administration/logs.types';
 
 @Component({
   selector: 'app-logs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    SorterComponent,
+    MatTableModule,
+  ],
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.scss']
 })
@@ -19,10 +28,22 @@ export class LogsComponent implements OnInit {
     
   }
 
+  logs: IIndexedLogActivity[] = [
+    {
+      index: 0,
+      date: Date.now().toString(),
+      message: '',
+    }
+  ]
+
   page = 0;
   order: order = 'asc';
+
+  total = 0;
 
   updateLogs() {
 
   }
+
+  protected readonly displayedColumns = ['index', 'message', 'date'];
 }
