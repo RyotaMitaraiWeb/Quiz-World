b49bcbf5b20db82bb21b0f34f7b8cc057fe66e11
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-08-10 14:21:12 +0300

    fix test

--- src/app/features/home/home.component.spec.ts
+++ src/app/features/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { AppStoreModule } from '../../store/app-store.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +10,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent, AppStoreModule, RouterTestingModule]
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
