a6c0a0472ecd99b053e3199c0f66bf0321b70478
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2025-01-17 21:57:44 +0200

    migrate session check (#54)
    * create token interceptor
    
    * implement session check on app load
    
    * add tests and remove unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth/auth.service';
+import { UserStore } from './store/user/user.store';
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,21 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'quizworld';
+export class AppComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+  private readonly userStore = inject(UserStore);
+
+  ngOnInit(): void {
+    this.authService.retrieveSession().subscribe({
+      next: (result) => {
+        const { username, id, roles } = result;
+        this.userStore.updateUser({ username, id, roles });
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err.status === HttpStatusCode.Unauthorized) {
+          localStorage.removeItem('token');
+        }
+      },
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './interceptors/token/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideExperimentalZonelessChangeDetection()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideExperimentalZonelessChangeDetection(),
+  ],
 
 };
