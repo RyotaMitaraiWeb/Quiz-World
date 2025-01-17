45f119a88909fd9d21e00aedafa449e5c6bf3042
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-17 21:48:34 +0200

    add tests and remove unnecessary code

--- src/app/interceptors/token/token.interceptor.ts
+++ src/app/interceptors/token/token.interceptor.ts
@@ -1,15 +1,6 @@
 import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
-import { determineIfUrlIsBlacklisted, UrlBlackist } from '../urlBlacklist';
-
-const urlBlacklist: UrlBlackist = [];
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const urlIsBlacklisted = determineIfUrlIsBlacklisted(req, urlBlacklist);
-
-  if (urlIsBlacklisted) {
-    return next(req);
-  }
-
   const token = localStorage.getItem('token');
   let headers = new HttpHeaders();
 
