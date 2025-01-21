b2e61d34dedc4aaf2492568f685ac1773025e82a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-22 00:02:27 +0200

    style the overview

--- src/app/components/quiz/quiz-overview/quiz-overview.component.ts
+++ src/app/components/quiz/quiz-overview/quiz-overview.component.ts
@@ -3,10 +3,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { DatePipe } from '@angular/common';
 import { QuizStore } from '../../../store/quiz/quiz.store';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-quiz-overview',
-  imports: [MatCardModule, MatChipsModule, DatePipe],
+  imports: [MatCardModule, MatChipsModule, DatePipe, MatIconModule],
   templateUrl: './quiz-overview.component.html',
   styleUrl: './quiz-overview.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
