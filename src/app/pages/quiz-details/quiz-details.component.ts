fe2174a2dfada2e2ea5cb7c63122e81294cc713a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-24 03:28:42 +0200

    set a title

--- src/app/pages/quiz-details/quiz-details.component.ts
+++ src/app/pages/quiz-details/quiz-details.component.ts
@@ -5,6 +5,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { QuizOverviewComponent } from '../../components/quiz/quiz-overview/quiz-overview.component';
 import { QuizGameComponent } from '../../components/quiz/quiz-game/quiz-game.component';
 import { QuizStore } from '../../store/quiz/quiz.store';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-quiz-details',
@@ -16,6 +17,8 @@ import { QuizStore } from '../../store/quiz/quiz.store';
 export class QuizDetailsComponent implements OnDestroy, AfterViewInit {
   private readonly quizService = inject(QuizService);
   protected readonly quiz = inject(QuizStore);
+  private readonly title = inject(Title);
+
   id = input.required<number>();
 
   @ViewChild(QuizGameComponent)
@@ -25,6 +28,7 @@ export class QuizDetailsComponent implements OnDestroy, AfterViewInit {
     this._quizSub = this.quizService.getById(this.id()).subscribe({
       next: (quiz) => {
         this.quiz.updateQuiz(quiz);
+        this.title.setTitle(`${quiz.title} quiz | Quiz World`);
         this.game.initialize(quiz);
       },
       error: () => {
