465c35306ced6916a8aad3b974341ac1a7803459
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-24 13:06:38 +0200

    add messages for required

--- src/app/common/validationErrors/quiz-form.ts
+++ src/app/common/validationErrors/quiz-form.ts
@@ -8,20 +8,24 @@ export const quizErrors = {
   title: {
     minlength: `Title must be at least ${title.minlength} characters long`,
     maxlength: `Title must be no longer than ${title.maxlength} characters long`,
+    required: 'Please provide a title for your quiz!',
   },
   description: {
     minlength: `Description must be at least ${description.minlength} characters long`,
     maxlength: `Description must be no longer than ${description.maxlength} characters long`,
+    required: 'Please provide a description for your quiz!',
   },
   questions: {
     prompt: {
       maxLength: `Prompt must be no longer than ${prompt.maxlength} characters long`,
+      required: 'Please provide a valid question!',
     },
     notes: {
       maxlength: `Notes must be no longer than ${notes.maxlength} characters long`,
     },
     answers: {
       maxlength: `Answers must be no longer than ${answers.maxlength} characters long`,
+      required: 'Please provide a valid answer!',
     },
   },
 };
