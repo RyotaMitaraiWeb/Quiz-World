7ffa8225a2df6f332335941b4bde6726358ed891
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-27 15:09:30 +0200

    set input

--- src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.spec.ts
+++ src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.spec.ts
@@ -2,6 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuizFormBasicDataComponent } from './quiz-form-basic-data.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { QuizBasicDataForm } from '../types';
+import { FormControl, FormGroup } from '@angular/forms';
+
+const form: QuizBasicDataForm = new FormGroup(
+  {
+    title: new FormControl(''),
+    description: new FormControl(''),
+    instantMode: new FormControl(false),
+  },
+);
 
 describe('QuizFormBasicDataComponent', () => {
   let component: QuizFormBasicDataComponent;
@@ -15,6 +25,7 @@ describe('QuizFormBasicDataComponent', () => {
 
     fixture = TestBed.createComponent(QuizFormBasicDataComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('form', form);
     fixture.detectChanges();
   });
 
