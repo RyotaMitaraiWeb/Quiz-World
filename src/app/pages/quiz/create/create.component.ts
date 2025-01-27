4b17b5b98536380eba014795405feaebc866c288
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-27 15:32:20 +0200

    add some style to create page

--- src/app/pages/quiz/create/create.component.ts
+++ src/app/pages/quiz/create/create.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { QuizFormComponent } from '../../../components/quiz/quiz-form/quiz-form.component';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-create',
-  imports: [QuizFormComponent],
+  imports: [QuizFormComponent, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
