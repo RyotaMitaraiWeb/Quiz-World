c5306e33e44067ab38292abf0f78ba2837727c12
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-27 15:05:37 +0200

    fix default tests

--- src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.spec.ts
+++ src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuizFormBasicDataComponent } from './quiz-form-basic-data.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('QuizFormBasicDataComponent', () => {
   let component: QuizFormBasicDataComponent;
@@ -8,7 +9,7 @@ describe('QuizFormBasicDataComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [QuizFormBasicDataComponent]
+      imports: [QuizFormBasicDataComponent, NoopAnimationsModule],
     })
     .compileComponents();
 
