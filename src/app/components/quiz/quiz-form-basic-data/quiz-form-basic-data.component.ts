1625ac58fcd83baf4917388e8d6fefcfd9669364
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-27 16:16:08 +0200

    style the first section

--- src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.ts
+++ src/app/components/quiz/quiz-form-basic-data/quiz-form-basic-data.component.ts
@@ -5,6 +5,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { SingleInputErrorPipe } from '../../../pipes/single-input-error/single-input-error.pipe';
+import { quizErrors } from '../../../common/validationErrors/quiz-form';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { quizValidationRules } from '../../../common/validationRules/quiz-form';
 
 @Component({
   selector: 'app-quiz-form-basic-data',
@@ -14,6 +18,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatInputModule,
     MatCheckboxModule,
+    CdkTextareaAutosize,
+    SingleInputErrorPipe,
   ],
   templateUrl: './quiz-form-basic-data.component.html',
   styleUrl: './quiz-form-basic-data.component.scss',
@@ -21,4 +27,8 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class QuizFormBasicDataComponent {
   form = input.required<QuizBasicDataForm>();
+
+  protected titleErrorMessages = quizErrors.title;
+  protected descriptionErrorMessages = quizErrors.description;
+  protected maxDescriptionLength = quizValidationRules.description.maxlength;
 }
