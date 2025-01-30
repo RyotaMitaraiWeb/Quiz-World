2408f134b28b2a06ba472001d658f0b26a8fcf7e
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-01-30 23:07:23 +0200

    fix faulty tests

--- src/app/pages/quiz/create/create.component.spec.ts
+++ src/app/pages/quiz/create/create.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateQuizComponent } from './create.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CreateComponent', () => {
   let component: CreateQuizComponent;
@@ -10,6 +12,7 @@ describe('CreateComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CreateQuizComponent, NoopAnimationsModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     })
     .compileComponents();
 
--- src/app/pages/quiz/edit/edit.component.spec.ts
+++ src/app/pages/quiz/edit/edit.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditComponent } from './edit.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('EditComponent', () => {
   let component: EditComponent;
@@ -8,12 +11,14 @@ describe('EditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditComponent]
+      imports: [EditComponent, NoopAnimationsModule],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(EditComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('id', 1);
     fixture.detectChanges();
   });
 
