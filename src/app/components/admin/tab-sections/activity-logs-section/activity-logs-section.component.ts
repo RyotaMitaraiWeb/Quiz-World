8cbe15a86ef637bbf7dd13702842eeed1dfba1ce
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-11 00:02:26 +0300

    make activity logs section fully functional

--- src/app/components/admin/tab-sections/activity-logs-section/activity-logs-section.component.ts
+++ src/app/components/admin/tab-sections/activity-logs-section/activity-logs-section.component.ts
@@ -3,9 +3,11 @@ import { AdminService } from '../../../../services/admin/admin.service';
 import { MatSelectModule } from '@angular/material/select';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { order, sorting } from '../../../../common/sort';
-import { debounceTime, distinctUntilChanged, of, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { IndexedLogsList } from '../../../../services/admin/logs.types';
 import { MatTableModule } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-activity-logs-section',
@@ -13,6 +15,8 @@ import { MatTableModule } from '@angular/material/table';
     ReactiveFormsModule,
     MatSelectModule,
     MatTableModule,
+    MatPaginatorModule,
+    DatePipe,
   ],
   templateUrl: './activity-logs-section.component.html',
   styleUrl: './activity-logs-section.component.scss',
@@ -20,9 +24,14 @@ import { MatTableModule } from '@angular/material/table';
 })
 export class ActivityLogsSectionComponent implements OnInit, OnDestroy {
   private readonly adminService = inject(AdminService);
+  protected readonly displayedColumns = ['index', 'message', 'date'];
 
   ngOnInit() {
     this.form.controls.page.setValue(1);
+  }
+
+  changePage(page: number) {
+    this.form.controls.page.setValue(page);
   }
 
   logs = signal<IndexedLogsList>({
@@ -46,11 +55,10 @@ export class ActivityLogsSectionComponent implements OnInit, OnDestroy {
   private _logsSub = this.form.valueChanges.pipe(
     debounceTime(500),
     distinctUntilChanged(),
-    switchMap(() => of(this.form.value)),
-    switchMap((value) => this.adminService.getActivityLogs(
+    switchMap(() => this.adminService.getActivityLogs(
       {
-        page: value.page || 1,
-        order: value.order || sorting.order[0],
+        page: this.form.value.page || 1,
+        order: this.form.value.order || sorting.order[0],
       },
     )),
   ).subscribe({
