a005b0176674f2c25787690560c364547f247e38
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-11 18:17:12 +0300

    create desktop navigation

---
+++ src/app/components/layout/navigation/desktop-navigation/desktop-navigation.component.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DesktopSearchFieldComponent } from '../desktop-search-field/desktop-search-field.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'app-desktop-navigation',
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    DesktopSearchFieldComponent,
+  ],
+  templateUrl: './desktop-navigation.component.html',
+  styleUrl: './desktop-navigation.component.scss',
+})
+export class DesktopNavigationComponent {
+  protected open = signal(false);
+
+  toggleMenu() {
+    this.open.update(o => !o);
+  }
+}
