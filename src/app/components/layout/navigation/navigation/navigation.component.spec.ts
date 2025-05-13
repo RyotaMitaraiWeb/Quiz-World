c6d8ef163c80dfaf346a665796ae1c18f3c174e6
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-13 16:45:38 +0300

    fix faulty test

--- src/app/components/layout/navigation/navigation/navigation.component.spec.ts
+++ src/app/components/layout/navigation/navigation/navigation.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -8,7 +9,7 @@ describe('NavigationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent],
+      imports: [NavigationComponent, NoopAnimationsModule],
     })
     .compileComponents();
 
