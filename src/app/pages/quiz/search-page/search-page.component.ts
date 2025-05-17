b49de6fe7602495c03df28782f2890828efc6fa3
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-17 18:46:20 +0300

    create a semifunctional search page

--- src/app/pages/quiz/search-page/search-page.component.ts
+++ src/app/pages/quiz/search-page/search-page.component.ts
@@ -1,9 +1,11 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, map } from 'rxjs';
-import { order, quizSort, SortAndOrder } from '../../../common/sort';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, map, Subscription, switchMap } from 'rxjs';
 import { SearchResultsComponent } from '../../../components/search/search-results/search-results.component';
+import { SearchResultsService } from '../../../services/search-results/search-results.service';
+import { QuizService } from '../../../services/quiz/quiz.service';
+import { QuizList } from '../../../services/quiz/types';
 
 @Component({
   selector: 'app-search-page',
@@ -13,35 +15,50 @@ import { SearchResultsComponent } from '../../../components/search/search-result
 ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnDestroy, OnInit {
   private readonly activatedRoute = inject(ActivatedRoute);
-  private readonly router = inject(Router);
+  private readonly quizService = inject(QuizService);
   private readonly queryParams = this.activatedRoute.queryParamMap;
+  private readonly searchResults = inject(SearchResultsService);
 
   protected readonly searchQuery = this.queryParams.pipe(
-    map(qp => qp.get('query')),
+    map(qp => qp.get('query') || ''),
   );
 
-  search(options: SortAndOrder) {
-    this.router.navigate(['quiz', 'search'], {
-      queryParams: options,
-      queryParamsHandling: 'merge',
+  quizList = signal<QuizList>(
+    {
+      total: 0,
+      quizzes: [],
+    },
+  );
+
+  private readonly searchParams = combineLatest([this.searchQuery, this.searchResults.searchOptions$])
+    .pipe(
+      map(value => (
+        { ...value[1], search: value[0] }
+      ),
+  ));
+
+  ngOnInit() {
+    this.searchResultsSub = this.searchParams.pipe(
+      switchMap((params) => (
+        this.quizService.getQuizzesByTitle(params.search, {...params })
+      )),
+    ).subscribe({
+      next: (value) => {
+        this.quizList.set(value);
+      },
+      error() {
+        //
+      },
     });
   }
 
-  private readonly _orderQuery$ = this.queryParams.pipe(
-    map(qp => qp.get('order') as order),
-  );
+  ngOnDestroy() {
+    this.searchResultsSub?.unsubscribe();
+  }
 
-  private readonly _sortQuery$ = this.queryParams.pipe(
-    map(qp => qp.get('sort') as quizSort),
-  );
-
-  protected readonly searchOptions$ = combineLatest(
-      [this._orderQuery$, this._sortQuery$],
-    )
-    .pipe(
-      map(options => ({ sort: options[1], order: options[0] })),
-    );
+  searchResultsSub?: Subscription;
 }
