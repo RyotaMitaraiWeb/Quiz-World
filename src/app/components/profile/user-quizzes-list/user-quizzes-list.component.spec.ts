af5964bbf37641f7a1df3b6cc230fe2f004d2d4c
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-05-19 01:18:18 +0300

    create a tab for user quizzes

---
+++ src/app/components/profile/user-quizzes-list/user-quizzes-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserQuizzesListComponent } from './user-quizzes-list.component';
+
+describe('UserQuizzesListComponent', () => {
+  let component: UserQuizzesListComponent;
+  let fixture: ComponentFixture<UserQuizzesListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserQuizzesListComponent],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserQuizzesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
