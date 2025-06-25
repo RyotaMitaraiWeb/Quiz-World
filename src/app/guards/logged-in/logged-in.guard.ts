6333a50adb54307e7ac73e676ab52679476f25b1
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-25 20:16:00 +0300

    add a snackbar when guard fails

--- src/app/guards/logged-in/logged-in.guard.ts
+++ src/app/guards/logged-in/logged-in.guard.ts
@@ -1,12 +1,19 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserStore } from '../../store/user/user.store';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SNACKBAR_DURATION, snackbarAction, snackbarMessages } from '../../common/snackbar';
 
 export const loggedInGuard: CanActivateFn = () => {
   const userStore = inject(UserStore);
   const router = inject(Router);
+  const snackbar = inject(MatSnackBar);
   const isLoggedIn = userStore.isLoggedIn();
   if (isLoggedIn) return true;
+
+  snackbar.open(snackbarMessages.error.login, snackbarAction, {
+    duration: SNACKBAR_DURATION,
+  });
 
   return router.parseUrl('/auth/login');
 };
